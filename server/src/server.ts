dd594a739025030953be49290a580dcc73202789
João Marcos <joao.marcos@inovarymktdigital.com.br>
2020-06-04 23:33:18 -0300

    Preparando o ambiente Next Level Week

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'João',
+        'Marcos'
+    ]);
+});
+
+app.listen(3333);
